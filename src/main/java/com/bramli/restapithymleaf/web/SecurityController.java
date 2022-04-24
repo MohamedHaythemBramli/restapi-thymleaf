6a79bb181dc0ff739c81459ee3d91050cad821ff
MohamedHaythemBramli <mohamedhaythembramli90@gmail.com>
2022-04-24 17:59:51 +0000

    sec2

--- src/main/java/com/bramli/restapithymleaf/web/SecurityController.java
+++ src/main/java/com/bramli/restapithymleaf/web/SecurityController.java
@@ -1,2 +1,12 @@
-package com.bramli.restapithymleaf.web;public class SecurityController {
+package com.bramli.restapithymleaf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+    @GetMapping("/403")
+    public String notAuthorized(){
+        return "403";
+    }
 }
